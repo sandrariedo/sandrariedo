42e019110f2ba873c3cc8989e523a0fe8ed48b5d
sandra <sandrariedo@yahoo.com.br>
2022-09-27 15:00:44 -0400

    Msg

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -2,7 +2,7 @@ public class Person {
     private final String name;
     private final int age;
 
-    // construtor
+    // construtor xxx
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
