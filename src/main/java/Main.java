590c04c168e779e1b78520a8b46565d3836b0a11
lunarosa <sandrariedo@yahoo.com.br>
2022-09-27 09:24:28 -0400

    Commit All files

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Person person = new Person("Luna");
+        System.out.println(person.getName());
+    }
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,36 @@
+public class Person {
+    private final String name;
+    private final int age;
+
+    // construtor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // getter
+    public String getName() {
+        return name;
+    }
+
+    // setter
+    public int getAge() {
+        return age;
+    }
+
+
+    void eat () {
+        System.out.println((this.name+ " is eating"));
+    }
+    void drink () {
+        System.out.println((this.name+ " is drinking"));
+    }
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
