2cb06d8b50c751f90ef80d69f09661d297069b03
sandra <sandrariedo@yahoo.com.br>
2022-10-03 16:28:31 -0400

    Branch Feature 01

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,9 @@ public class Main {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Person person = new Person("Luna");
-        System.out.println(person.getName());
+        Person person1 = new Person("Luna");
+        Person person2 = new Person("Rosa");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
     }
 }
